c07a04c1cbb8a5c45c2a0d45ae9e90e80535179d
martin0123478 <martin_agui97@outlook.es>
2021-12-21 22:37:54 -0600

    diseño de formularios de login y registro con formularios reactivos

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+form:FormGroup = this.fb.group({
+  email:['test1@tes.com',[Validators.required,Validators.email]],
+  password:['123456',[Validators.required,Validators.minLength(6)]],
+})
+  constructor(
+    private fb:FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
+login(){
+  console.log(this.form.value)
+  console.log(this.form.valid)
 
+}
 }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class RegisterComponent implements OnInit {
+form:FormGroup = this.fb.group({
+  name:['test4',[Validators.required]],
+  email:['test4@test.com',[Validators.required,Validators.email]],
+  password:['123456',[Validators.required,Validators.minLength(6)]]
 
-  constructor() { }
+
+})
+  constructor(
+    private fb:FormBuilder
+  ) { }
 
   ngOnInit(): void {
+  }
+  registrar(){
+    console.log(this.form.value)
+    console.log(this.form.valid)
   }
 
 }
